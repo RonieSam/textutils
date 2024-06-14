60c72e87bd7ab8df7b0f0f85bce8565b90a2b1dc
RonieSam <84262942+RonieSam@users.noreply.github.com>
2024-06-14 12:43:13 +0530

    added a about page using react router

--- src/App.js
+++ src/App.js
@@ -2,26 +2,45 @@ import { useState } from 'react';
 import './App.css';
 import NavBar from './components/navbar';
 import TextArea from './components/textArea';
-// import About from './components/abou';
+import About from './components/about';
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes,
+ 
+} from "react-router-dom"
 function App() {
   const [mode,setMode]=useState("Light")
+  const [colorMode,setColor]=useState("black")
   const toggleMode=()=>{
     if(mode==="Dark"){
       setMode("Light")
       document.body.style.backgroundColor="white"
+      setColor("black")
     }
     else{
       setMode("Dark")
       document.body.style.backgroundColor="black"
+      setColor("white")
 
     }
   }
   return (
-    < >
-      <NavBar title="TextUtils" text="Home" mode={mode} toggleMode={toggleMode}/>
-      <div className='container'><TextArea mode={mode}/></div>
-      {/* <About/> */}
+    <Router>
+    < > 
+    <NavBar title="TextUtils" text="About" mode={mode} toggleMode={toggleMode}/>
+    <Routes>
+      <Route path="/" element={<TextArea mode={mode} colorMode={colorMode}/>}>
+           
+
+      </Route>
+      <Route path="/about" element={<About mode={mode}/>}>
+            
+      </Route>
+    </Routes>
     </>
+
+    </Router>
   );
 }
 
--- src/components/textArea.js
+++ src/components/textArea.js
@@ -3,12 +3,13 @@ import { useState } from "react";
 
 export default function TextArea(props) {
   const [wordCount,setWordCount]=useState(0)
-  const [text,setText]=useState("this is the state")
+  const [text,setText]=useState("")
   const handleOnChange=(event)=>{
     setText(event.target.value)
-    const wordArr=text.split(" ")
-    let wordArrWOSpc=wordArr.filter((ele)=>ele!==" ")
-    setWordCount(wordArrWOSpc.length?wordArrWOSpc.length:0)
+    const wordArr=event.target.value.split(" ")
+    let wordArrWOSpc=wordArr.filter((ele)=>{
+       return ele!==''})
+    setWordCount(wordArrWOSpc.length)
  }
   
   const handleOnClickUC=()=>{
@@ -16,20 +17,21 @@ export default function TextArea(props) {
   }
   const handleOnClickCT=()=>{
     setText("")
-    console.log(text.length)
+    setWordCount(0)
   }
  
   
   return (
     <>
-<div className="mb-3 my-4" data-bs-theme={props.mode.toLowerCase()} >
+<div className="mb-3 mx-5 my-4" data-bs-theme={props.mode.toLowerCase()} >
   <label htmlFor="exampleFormControlTextarea1" className="form-label my-4">{props.heading}</label>
-  <textarea className="form-control" id="exampleFormControlTextarea1" rows="8" value={text} onChange={handleOnChange} style={{borderWidth:1,borderColor:(props.mode==="Light")?"black":"", margin:5}} />
-  <button className={`btn btn-${props.mode.toLowerCase()} my-4 mx-2`}  onClick={handleOnClickUC} style={{borderWidth:1,borderColor:(props.mode==="Light")?"black":"", margin:5}}>Submit</button>
-  <button className={`btn btn-${props.mode.toLowerCase()} my-4 mx-2`}  onClick={handleOnClickCT} style={{borderWidth:1,borderColor:(props.mode==="Light")?"black":""}}>Clear Text</button>
+  <textarea className="form-control" id="exampleFormControlTextarea1" rows="8" value={text} onChange={handleOnChange} style={{borderWidth:1,borderColor:props.colorMode}} />
+  <button className={`btn btn-${props.mode.toLowerCase()} my-4 mx-2`}  onClick={handleOnClickUC} style={{borderWidth:0.5,borderColor:props.colorMode}}>Submit</button>
+  <button className={`btn btn-${props.mode.toLowerCase()} my-4 mx-2`}  onClick={handleOnClickCT} style={{borderWidth:0.5,borderColor:props.colorMode}}>Clear Text</button>
 
-  <p style={{color:(props.mode==="Light")?"black":"white"}}>Words-{wordCount} Character-{text.length}</p>
+  <p style={{color:props.colorMode}}>Words-{wordCount} Character-{text.length}</p>
 </div>
     </>
   )
 }
+
