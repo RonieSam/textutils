394f7e7a48823fccbc56aca474b477d03a641034
RonieSam <84262942+RonieSam@users.noreply.github.com>
2024-06-15 08:15:13 +0530

    added copy text button and fixed the word counter

--- src/components/textArea.js
+++ src/components/textArea.js
@@ -6,9 +6,11 @@ export default function TextArea(props) {
   const [text,setText]=useState("")
   const handleOnChange=(event)=>{
     setText(event.target.value)
-    const wordArr=event.target.value.split(" ")
-    let wordArrWOSpc=wordArr.filter((ele)=>{
-       return ele!==''})
+    const wordArr=event.target.value.trim().split(/\s+/);
+    console.log(wordArr)
+     let wordArrWOSpc=wordArr.filter((ele)=>{
+        return ele!==""})
+    
     setWordCount(wordArrWOSpc.length)
  }
   
@@ -19,15 +21,21 @@ export default function TextArea(props) {
     setText("")
     setWordCount(0)
   }
+  const handleClickCopy=()=>{
+    navigator.clipboard.writeText(text)
+
+  }
  
   
   return (
     <>
-<div className="mb-3 mx-5 my-4" data-bs-theme={props.mode.toLowerCase()} >
+<div className="mb-3 mx-5 my-4" data-bs-theme={props.mode.toLowerCase()}   >
   <label htmlFor="exampleFormControlTextarea1" className="form-label my-4">{props.heading}</label>
   <textarea className="form-control" id="exampleFormControlTextarea1" rows="8" value={text} onChange={handleOnChange} style={{borderWidth:1,borderColor:props.colorMode}} />
   <button className={`btn btn-${props.mode.toLowerCase()} my-4 mx-2`}  onClick={handleOnClickUC} style={{borderWidth:0.5,borderColor:props.colorMode}}>Submit</button>
   <button className={`btn btn-${props.mode.toLowerCase()} my-4 mx-2`}  onClick={handleOnClickCT} style={{borderWidth:0.5,borderColor:props.colorMode}}>Clear Text</button>
+  <button className={`btn btn-${props.mode.toLowerCase()} my-4 mx-2`}  onClick={handleClickCopy} style={{borderWidth:0.5,borderColor:props.colorMode}}>Copy Text</button>
+
 
   <p style={{color:props.colorMode}}>Words-{wordCount} Character-{text.length}</p>
 </div>
